ec67fafbedbb114fa45934cbf2a18dcdb2a43f0c
hawkeyexl <hawkeyexl@gmail.com>
2026-01-24 14:50:52 -0800

    chore: Remove old Bun build scripts and intermediate artifacts
    - build-scripts.sh (both skills) - replaced by build-wasm.sh
    - inject-inline.bundled.js - intermediate esbuild output not needed in repo

--- skills/inline-test-injection/scripts/build-scripts.sh
+++
@@ -1,3 +0,0 @@
-bun --version
-cd src
-bun build inject-inline.mjs --compile --outfile ../dist/inline-test-injection
--- skills/inline-test-injection/scripts/dist/inject-inline.bundled.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:463f5c615f8aaf9f10fd61f5363548153807a47236f7c1aa6d77868389a57e77
-size 22160
